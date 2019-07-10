3264e5509d6ad6139f3d2364a591d363edc4abbf
Mohammad-Alaa-Aldein <51743463+Mohammad-Alaa-Aldein@users.noreply.github.com>
2019-07-10 12:33:27 +0300

    Add files via upload

---
+++ quiz/all-tide-up.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: All Tied Up 
+ * Build a single string that resembles the following joke.
+ *
+ * Why couldn't the shoes go out and play?
+ * They were all "tied" up!
+ * Your joke should take the format of a question and answer. 
+ * The first line should be a question and the second line should be an answer.
+ * Hint: You will need to use special characters to produce the following output.*/
+
+
+
+	let question=("Why couldn t the shoes go out and play?");
+	let answer=("They were all "tied" up!");
+
+	console.log(question);
+	console.log(answer);
---
+++ quiz/expression.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: First Expression 
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+
+var sum1=( 4 * 10 + 2 );
+var sum2=( 5 * 8 + 2 );
+var sum3=( 22 * 2 - 2 );
+
+console.log( sum1, sum2, sum3 )
